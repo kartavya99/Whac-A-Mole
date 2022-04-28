692b3da25bf7837e0cd6e215cb45520ac3f6a6dc
kartavya <kartavyas@gmail.com>
2022-04-28 22:42:02 +1000

    JS updated with mole movement

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@ const square = document.querySelectorAll(".square");
 const mole = document.querySelectorAll(".mole");
 let score = document.querySelector("#score");
 let moleTimerId;
+
+
+function randomSquare() {
+    
+    square.forEach(square => {
+        square.classList.remove('mole');
+    })
+    
+    let  randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add("mole");
+}
+
+function moveMole(){
+    moleTimerId = setInterval(randomSquare, 1000);
+}
+
+moveMole();
